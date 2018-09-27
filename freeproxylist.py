b47a934781f815244f4042f30077198882ede5bd
eeghor <eeghor@gmail.com>
2018-09-27 11:54:11 +1000

    fully working

--- freeproxylist.py
+++ freeproxylist.py
@@ -1,14 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+import re
 import time
-
-options = webdriver.ChromeOptions()
-options.add_argument('disable-notifications')
-# options.add_argument('headless')
 
 """
 Level 1 - Elite Proxy / Highly Anonymous Proxy: The web server can't detect whether you are using a proxy.
@@ -19,68 +14,89 @@ Level 3 - Transparent Proxy: The web server can know you are using a proxy and i
 
 class FreeProxies:
 
-	DRIVER_ = webdriver.Chrome('webdriver/chromedriver', chrome_options=options)
 	BASE_URL_ = 'https://free-proxy-list.net'
 
-	def __init__(self, anonymity='elite proxy'):
+	def __init__(self, anonymity='elite proxy', wait=30):
 
 		assert anonymity in ['elite proxy', 'transparent', 'anonymous'], print('choose correct anonymity')
 
 		self.anonymity = anonymity
+		self.wait  = wait
 
 		self.lst = []
 
 	def get(self):
 
-		FreeProxies.DRIVER_.get(FreeProxies.BASE_URL_)
+		options = webdriver.ChromeOptions()
+		options.add_argument('disable-notifications')
+		options.add_argument('headless')
+
+		self.driver = webdriver.Chrome('webdriver/chromedriver', chrome_options=options)
+
+		self.driver.get(FreeProxies.BASE_URL_)
 		
-		WebDriverWait(FreeProxies.DRIVER_, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 
-			f'#proxylisttable > tfoot > tr > th:nth-child(5) > select>option[value="{self.anonymity}"]'))).click()
+		WebDriverWait(self.driver, self.wait).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 
+				f'#proxylisttable>tfoot>tr>th:nth-child(5)>select>option[value="{self.anonymity}"]'))).click()
+		
 		time.sleep(3)
 
-		print(WebDriverWait(FreeProxies.DRIVER_, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 
-			'#proxylisttable_info'))).text)
+		info_ = WebDriverWait(self.driver, self.wait).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 
+									'#proxylisttable_info'))).text.strip().lower()
 
-		page_numbers = [int(_.text.strip()) for _ in FreeProxies.DRIVER_.find_elements_by_css_selector('#proxylisttable_paginate > ul > li') 
+		nres = int(re.search(r'(?<=of)\s+\d+\s+(?=entries)', info_).group(0).strip())  #  of 145 entries
+
+		print(f'found {nres} proxies')
+
+		page_numbers = [int(_.text.strip()) for _ in self.driver.find_elements_by_css_selector('#proxylisttable_paginate>ul>li') 
 							if _.text.strip().isdigit()]
 
-		for page in range(max(page_numbers)):
+		print('collecting...')
 
-			print('page ', page)
+		for page in range(1, max(page_numbers) + 1):
 
-			# collect 
+			row_ = 1
 
 			while True:
 
-				row_ = 1
-
 				try:
-
-					self.lst.append(FreeProxies.DRIVER_.find_element_by_css_selector(f'#proxylisttable > tbody > tr:nth-child({row_})').text)
+					row_text = self.driver.find_element_by_css_selector(f'#proxylisttable>tbody>tr:nth-child({row_})').text.strip()
+					
+					if row_text not in self.lst:
+						self.lst.append(row_text)
 					row_ += 1
 				
 				except:	
-
-					for a in FreeProxies.DRIVER_.find_elements_by_css_selector('#proxylisttable_paginate >ul> li.fg-button.ui-button.ui-state-default>a'):
+					# can't get a row - time to go to the next page
+					for a in self.driver.find_elements_by_css_selector('#proxylisttable_paginate>ul>li.fg-button.ui-button.ui-state-default>a'):
 	
 						try:
-							next_page = int(a.text.strip())
-							if next_page == page + 1:
+							if int(a.text.strip()) == page + 1:
 								a.click()
 								time.sleep(5)
 								break
 						except:
 							continue
-	
 					break
 
-		print(f'total proxies: {len(self.lst)}')
+		if len(self.lst) != nres:
+			print(f'collected not as many proxies as expected: {len(self.lst)}!')
+		else:
+			print('done')
 
+		self.driver.close()
+
+		return self
+
+	def save(self, to_where='proxies.txt'):
+
+		with open(to_where, 'w') as f:	
+			for _ in self.lst:
+				f.write('ip: {} port: {}\n'.format(*_.split()[:2]))
+
+		print(f'saved to {to_where}')
+
+		return self
 
 if __name__ == '__main__':
 
-	FreeProxies().get()
-
-
-
-
+	FreeProxies().get().save()
